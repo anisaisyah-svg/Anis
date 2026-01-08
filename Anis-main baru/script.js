93f569ba324308dd31ba0a573af8ef138754443b
anisaisyah-svg <anisaisyahroslan@gmail.com>
2026-01-08 13:30:52 +0800

    Add files via upload
    Signed-off-by: anisaisyah-svg <anisaisyahroslan@gmail.com>

---
+++ Anis-main baru/script.js
@@ -0,0 +1,37 @@
+<script>
+alert("Selamat Datang");
+function mula() {
+    alert ("Boleh start isi borang. Tunggu apa lagi");
+	}
+function hantaq (){
+    var nama = document.getElementById("Name").value;
+	var fon = document.getElementById("Phone").value;
+	var email = document.getElementById("Email").value;
+	var comp = document.getElementById("complain").value;
+	
+	alert ("Terima kasih " + nama +" kami akan hubungi anda di " + fon);
+	
+	document.getElementById("result").innerHTML = "Terima kasih " + nama + 
+	"kami akan hubungi anda di" + fon + "atau" + email;
+	}
+function darkmode(){
+   document.getElementById("first-page").style.color="red";
+   document.getElementById("first-page").style.backgroundColor="darkblue";
+}
+
+function lightmode(){
+   document.getElementById("first-page").style.color="black";
+   document.getElementById("first-page").style.backgroundColor="#ADD8E6";
+}
+      
+function LargeText(){
+   document.getElementById("first-page").style.fontSize="200%";}
+   
+function SmallText(){
+   document.getElementById("first-page").style.fontSize="50%";}
+   
+function welc(){
+   document.getElementById("welcome").style.backgroundColor="#ffffed";} 
+   
+function borang(){
+   document.getElementById("form").style.backgroundColor="#ffe4e1";}   
